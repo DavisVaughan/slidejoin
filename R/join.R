d53c950fc747ee8181c726f383e036e753569718
DavisVaughan <davis@rstudio.com>
2021-05-12 12:35:09 -0400

    Reverse interpretation of `x` and `y` to better match dplyr

--- R/join.R
+++ R/join.R
@@ -9,73 +9,73 @@ slide_join <- function(x, y, i, ..., type = "locf") {
   x_i <- x[[i]]
   y_i <- y[[i]]
 
-  x[[i]] <- NULL
+  y[[i]] <- NULL
 
   x_size <- vec_size(x)
   y_size <- vec_size(y)
 
-  x_front <- min(1L, x_size)
-  x_back <- x_size
+  y_front <- min(1L, y_size)
+  y_back <- y_size
 
-  x_empty <- x_size == 0L
+  y_empty <- y_size == 0L
 
   if (identical(type, "locf")) {
-    from <- vec_slice(x_i, -x_back)
-    to <- vec_slice(x_i, -x_front)
+    from <- vec_slice(y_i, -y_back)
+    to <- vec_slice(y_i, -y_front)
 
     # Hack, need `hop_index(.bounds = "[)")`, then we wouldn't do `to - 1L`
     to <- to - 1L
     to <- pmax(to, from)
 
-    if (x_empty) {
-      y_slicer_before <- seq_len(y_size)
-      y_slicer_after <- integer()
+    if (y_empty) {
+      x_slicer_before <- seq_len(x_size)
+      x_slicer_after <- integer()
     } else {
-      y_slicer_before <- which(y_i < vec_slice(x_i, x_front))
-      y_slicer_after <- which(y_i >= vec_slice(x_i, x_back))
+      x_slicer_before <- which(x_i < vec_slice(y_i, y_front))
+      x_slicer_after <- which(x_i >= vec_slice(y_i, y_back))
     }
 
-    x_slicer_before <- vec_rep(NA_integer_, length(y_slicer_before))
-    x_slicer_after <- vec_rep(x_back, length(y_slicer_after))
+    y_slicer_before <- vec_rep(NA_integer_, length(x_slicer_before))
+    y_slicer_after <- vec_rep(y_back, length(x_slicer_after))
 
-    x_slicer_start <- x_front
-    x_slicer_stop <- x_back - 1L
+    y_slicer_start <- y_front
+    y_slicer_stop <- y_back - 1L
   } else if (identical(type, "nocb")) {
-    to <- vec_slice(x_i, -x_front)
-    from <- vec_slice(x_i, -x_back)
+    to <- vec_slice(y_i, -y_front)
+    from <- vec_slice(y_i, -y_back)
 
     # Hack, need `hop_index(.bounds = "(]")`, then we wouldn't do `from + 1L`
     # maybe (]? Not sure, but matches <=, > used in slicers
     from <- from + 1L
     from <- pmin(from, to)
 
-    if (x_empty) {
-      y_slicer_before <- integer()
-      y_slicer_after <- seq_len(y_size)
+    if (y_empty) {
+      x_slicer_before <- integer()
+      x_slicer_after <- seq_len(x_size)
     } else {
-      y_slicer_before <- which(y_i <= vec_slice(x_i, x_front))
-      y_slicer_after <- which(y_i > vec_slice(x_i, x_back))
+      x_slicer_before <- which(x_i <= vec_slice(y_i, y_front))
+      x_slicer_after <- which(x_i > vec_slice(y_i, y_back))
     }
 
-    x_slicer_before <- vec_rep(x_front, length(y_slicer_before))
-    x_slicer_after <- vec_rep(NA_integer_, length(y_slicer_after))
+    y_slicer_before <- vec_rep(y_front, length(x_slicer_before))
+    y_slicer_after <- vec_rep(NA_integer_, length(x_slicer_after))
 
-    x_slicer_start <- x_front + 1L
-    x_slicer_stop <- x_back
+    y_slicer_start <- y_front + 1L
+    y_slicer_stop <- y_back
   } else {
     abort("Invalid `type`.")
   }
 
   locs_list <- slider::hop_index(
-    .x = seq_len(y_size),
-    .i = y_i,
+    .x = seq_len(x_size),
+    .i = x_i,
     .starts = from,
     .stops = to,
     .f = identity
   )
 
-  x_slicer <- vec_rep_each(seq2(x_slicer_start, x_slicer_stop), lengths(locs_list))
-  y_slicer <- unlist(locs_list, recursive = FALSE, use.names = FALSE)
+  x_slicer <- unlist(locs_list, recursive = FALSE, use.names = FALSE)
+  y_slicer <- vec_rep_each(seq2(y_slicer_start, y_slicer_stop), lengths(locs_list))
 
   x_slicer <- c(x_slicer_before, x_slicer, x_slicer_after)
   y_slicer <- c(y_slicer_before, y_slicer, y_slicer_after)
@@ -83,8 +83,8 @@ slide_join <- function(x, y, i, ..., type = "locf") {
   x_out <- vec_slice(x, x_slicer)
   y_out <- vec_slice(y, y_slicer)
 
-  i_out <- y_out[, i, drop = FALSE]
-  y_out[[i]] <- NULL
+  i_out <- x_out[, i, drop = FALSE]
+  x_out[[i]] <- NULL
 
   # Probably needs to pass through names handling
   vec_cbind(
